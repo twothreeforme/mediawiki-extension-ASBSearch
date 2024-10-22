51cc6b0d43e04eb8dbbc01c4b73ab4cd6483b8da
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2024-10-22 19:48:58 -0400

    major overhaul... adding dynamic content for equipsets

--- includes/js/FFXIPackageHelper_dynamicContent.js
+++ includes/js/FFXIPackageHelper_dynamicContent.js
@@ -1,3 +1,68 @@
+// const SURVEY_ID = 1;
+
+console.log("loaded...");
+
+function runMe(){
+    console.log("HERE");
+}
+
+//mw.hook('wikipage.content').add(runMe());
+$( document ).ready( function () {
+    runMe();
+} );
 
 
-console.log("dynamicContent loaded !!!");
+
+//////SURVEY
+
+
+// const surveyJson = {
+//     elements: [{
+//         name: "FirstName",
+//         title: "Enter your first name:",
+//         type: "text"
+//     }, {
+//         name: "LastName",
+//         title: "Enter your last name:",
+//         type: "text"
+//     }]
+// };
+
+// const survey = new Survey.Model(surveyJson);
+
+// function alertResults (sender) {
+//     const results = JSON.stringify(sender.data);
+//     alert(results);
+//     // saveSurveyResults(
+//     //     "https://your-web-service.com/" + SURVEY_ID,
+//     //     sender.data
+//     // )
+// }
+
+// survey.onComplete.add(alertResults);
+
+// document.addEventListener("DOMContentLoaded", function() {
+    
+//     survey.render(document.getElementById("surveyContainer"));
+// });
+
+
+// // function saveSurveyResults(url, json) {
+// //     fetch(url, {
+// //         method: 'POST',
+// //         headers: {
+// //             'Content-Type': 'application/json;charset=UTF-8'
+// //         },
+// //         body: JSON.stringify(json)
+// //     })
+// //     .then(response => {
+// //         if (response.ok) {
+// //             // Handle success
+// //         } else {
+// //             // Handle error
+// //         }
+// //     })
+// //     .catch(error => {
+// //         // Handle error
+// //     });
+// // }
