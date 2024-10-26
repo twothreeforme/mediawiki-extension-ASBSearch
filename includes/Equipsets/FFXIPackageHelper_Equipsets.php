d2be589076adcebf2317f73025e121c5c8a4fae6
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2024-10-26 07:44:33 -0400

    Update FFXIPackageHelper_Equipsets.php

--- includes/Equipsets/FFXIPackageHelper_Equipsets.php
+++ includes/Equipsets/FFXIPackageHelper_Equipsets.php
@@ -22,23 +22,96 @@ class FFXIPackageHelper_Equipsets  {
                 <table class="FFXIPackageHelper_Equipsets_table">
                     <tr>
                         <th></th>
+                        <th>Slot</th>
                         <th>Item</th>
                     </tr>
                     <tr>
                         <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Main</td>
                         <td>heasfdasdgagafad</td>
                     </tr>
                     <tr>
                         <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Change</button></td>
+                        <td>Sub</td>
                         <td>nefsdfsck</td>
                     </tr>
                     <tr>
                         <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Range</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Ammo</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Head</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Neck</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Ear1</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Ear2</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Body</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Hands</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Ring1</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Ring2</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                                        <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Back</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Waist</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Legs</td>
+                        <td>hansdfs sf sfds</td>
+                    </tr>
+                    <tr>
+                        <td><button class="FFXIPackageHelper_dynamiccontent_addbutton">Add</button></td>
+                        <td>Feet</td>
                         <td>hansdfs sf sfds</td>
                     </tr>
                 </table>
             </div>
-            <div class="FFXIPackageHelper_Equipsets_showset">show set data here</div>
+            <div class="FFXIPackageHelper_Equipsets_showset">
+                <p class="STR">STR</p>
+                <span>STR</span>
+
+            </div>
         </div>
             
         ';
