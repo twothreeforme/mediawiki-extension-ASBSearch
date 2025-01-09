5ae907092e1718b189f1f03c404d6027cf845899
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-01-08 21:39:38 -0500

    Update APIModuleEquipsets.php

--- includes/APIModules/APIModuleEquipsets.php
+++ includes/APIModules/APIModuleEquipsets.php
@@ -49,9 +49,9 @@ class APIModuleEquipsets extends ApiBase {
             $equipList = $db->getEquipment( $params['search'], $params['mlvl'], $params['slot']);
             $finalList = $dm->parseEquipment( $equipList, $params['mjob'] );
 
-            throw new Exception(json_encode($finalList));
-            //$result->addValue($params['action'], $params['querytype'], [$finalList, $params['slot']]);
-            $result->addValue($params['action'], $params['querytype'], $params['search'] );
+            //throw new Exception(json_encode($finalList));
+            $result->addValue($params['action'], $params['querytype'], [$finalList, $params['slot']]);
+            //$result->addValue($params['action'], $params['querytype'], $params['search'] );
         }
         else if ( $params['action'] == "equipsets_change" ) {
 
