77de253e5622caf292dd6d7421e26f704e412567
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-01-20 16:20:29 -0500

    Update APIModuleEquipsets.php

--- includes/APIModules/APIModuleEquipsets.php
+++ includes/APIModules/APIModuleEquipsets.php
@@ -67,9 +67,10 @@ class APIModuleEquipsets extends ApiBase {
             $updatedGrid = $tabEquipsets->updateGridItems($incomingEquipmentList);
 
             //throw new Exception ( json_encode($incomingEquipmentList) . "\n:::\n" . json_encode($updatedGrid));
-
+            throw new Exception ( ' here' );
+            
             $result->addValue($params['action'], "stats", $newStats->getStats() );
-            //$result->addValue($params['action'], "grid", $updatedGrid );
+            $result->addValue($params['action'], "grid", $updatedGrid );
 
             //if ( $params['sjob'] == 1  ) throw new Exception ( json_encode($incomingEquipmentList) ."::::\n". json_encode($updatedGrid) );
             //throw new Exception (json_encode($result));
