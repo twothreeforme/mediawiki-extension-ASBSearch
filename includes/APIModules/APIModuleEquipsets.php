9ee32fb2625b28beb2c6e9787ac3d7ba3db5043c
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-02-09 15:40:51 -0500

    Update APIModuleEquipsets.php

--- includes/APIModules/APIModuleEquipsets.php
+++ includes/APIModules/APIModuleEquipsets.php
@@ -74,7 +74,7 @@ class APIModuleEquipsets extends ApiBase {
             $tabEquipsets = new FFXIPackageHelper_Equipsets();
             $updatedGrid = $tabEquipsets->updateGridItems($incomingEquipmentList);
 
-            //throw new Exception ( json_encode($incomingEquipmentList) . "\n:::\n" . json_encode($updatedGrid));
+            throw new Exception ( json_encode($incomingEquipmentList) . "\n:::\n" . json_encode($updatedGrid));
             //throw new Exception ( ' here' );
 
             $statsEncoded = base64_encode(json_encode($newStats->getStats()));
