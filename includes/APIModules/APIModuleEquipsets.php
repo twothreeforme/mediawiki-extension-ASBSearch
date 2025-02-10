ccda8b1f7ab2a6f5b513179939089a742a4b52d7
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-02-09 21:33:46 -0500

    Update APIModuleEquipsets.php

--- includes/APIModules/APIModuleEquipsets.php
+++ includes/APIModules/APIModuleEquipsets.php
@@ -83,7 +83,7 @@ class APIModuleEquipsets extends ApiBase {
 
             $stats = $newStats->getStats();
 
-
+            throw new Exception ( "here" );
             //$encodedStats = json_encode($stats);
 
 
