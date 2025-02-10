aa7f1988156a8cc40ff3ee5a364d9b67ef54511a
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-02-09 21:00:17 -0500

    Update APIModuleEquipsets.php

--- includes/APIModules/APIModuleEquipsets.php
+++ includes/APIModules/APIModuleEquipsets.php
@@ -78,9 +78,10 @@ class APIModuleEquipsets extends ApiBase {
 
             //if ( !str_contains($equipmentString, "13280") ) throw new Exception ( json_encode($incomingEquipmentList) . "\n:::\n" . json_encode($newEquipmentArray));
 
-            if ( !str_contains($equipmentString, "13280") ) throw new Exception ( "here" );
+
 
             $encodedStats = json_encode($newStats->getStats());
+            if ( !str_contains($equipmentString, "13280") ) throw new Exception ( "here" );
 
             $statsEncoded = base64_encode($encodedStats);
             $statsURLSafe = urlencode($statsEncoded);
