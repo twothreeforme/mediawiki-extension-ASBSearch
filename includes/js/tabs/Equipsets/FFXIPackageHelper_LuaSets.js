1368f6a2583a345be92ddddedda34fcb2eca56da
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-02-11 22:08:21 -0500

    Update FFXIPackageHelper_LuaSets.js

--- includes/js/tabs/Equipsets/FFXIPackageHelper_LuaSets.js
+++ includes/js/tabs/Equipsets/FFXIPackageHelper_LuaSets.js
@@ -4,7 +4,7 @@ module.exports.adjustLuaSet = function (set) {
   if ( !set ) return;
 
   let luashitacast = "<h2>Luashitacast</h2><pre>SetNameHere = {\n";
-  let ashitacast = "<h2>AshitaCast (LegacyAC)</h2><pre>&ltset name=\"SetNameHere\"&gt\n";
+  let ashitacast = "<h2>AshitaCast (LegacyAC)</h2><pre>&lt;set name=\"SetNameHere\"&gt;\n";
 
 
   for (let e = 0; e <= 15; e++) {
@@ -12,15 +12,14 @@ module.exports.adjustLuaSet = function (set) {
     if ( set[e] != 0 ){
       let item = set[e].replace("\'", "\\\'");
       luashitacast += `\t${LAC_slotName(e)}\'${item}\',\n`;
-      ashitacast += AC_slotName(e, item) + `\n`;
+      ashitacast += AC_slotName(e, item) + ` \n`;
     }
   }
   console.log(ashitacast);
   luashitacast += "},</pre>";
-  ashitacast += "&lt/set&gt";
+  ashitacast += "&lt;/set&gt;";
 
-
-  document.getElementById("FFXIPackageHelper_Equipsets_showLuaSets").innerHTML = luashitacast + ashitacast;
+  document.getElementById("FFXIPackageHelper_Equipsets_showLuaSets").innerHTML = luashitacast; //+ ashitacast;
 }
 
 function LAC_slotName(slot){
@@ -46,21 +45,21 @@ function LAC_slotName(slot){
 
 function AC_slotName(slot,item){
   switch(slot){
-    case 0: return `\t&ltmain&gt${item}&lt/main&gt`;
-    case 1: return `\t&ltsub&gt${item}&lt/sub&gt`;
-    case 2: return `\t&ltrange&gt${item}&lt/range&gt`;
-    case 3: return  `\t&ltammo&gt${item}&lt/main&gt`;
-    case 4: return  `\t&lthead&gt${item}&lt/head&gt`;
-    case 5: return  `\t&ltneck&gt${item}&lt/neck&gt`;
-    case 6: return  `\t&ltear1&gt${item}&lt/ear1&gt`;
-    case 7: return  `\t&ltear2&gt${item}&lt/ear2&gt`;
-    case 8: return  `\t&ltbody&gt${item}&lt/body&gt`;
-    case 9: return  `\t&lthands&gt${item}&lt/hands&gt`;
-    case 10: return `\t&ltring1&gt${item}&lt/ring1&gt`;
-    case 11: return `\t&ltring2&gt${item}&lt/ring2&gt`;
-    case 12: return `\t&ltback&gt${item}&lt/back&gt`;
-    case 13: return `\t&ltwaist&gt${item}&lt/waist&gt`;
-    case 14: return `\t&ltlegs&gt${item}&lt/legs&gt`;
-    case 15: return `\t&ltfeet&gt${item}&lt/feet&gt`;
+    case 0: return `\t&lt;main&gt;${item}&lt;/main&gt;`;
+    case 1: return `\t&lt;sub&gt;${item}&lt;/sub&gt;`;
+    case 2: return `\t&lt;range&gt;${item}&lt;/range&gt;`;
+    case 3: return  `\t&lt;ammo&gt;${item}&lt;/main&gt;`;
+    case 4: return  `\t&lt;head&gt;${item}&lt;/head&gt;`;
+    case 5: return  `\t&lt;neck&gt;${item}&lt;/neck&gt;`;
+    case 6: return  `\t&lt;ear1&gt;${item}&lt;/ear1&gt;`;
+    case 7: return  `\t&lt;ear2&gt;${item}&lt;/ear2&gt;`;
+    case 8: return  `\t&lt;body&gt;${item}&lt;/body&gt;`;
+    case 9: return  `\t&lt;hands&gt;${item}&lt;/hands&gt;`;
+    case 10: return `\t&lt;ring1&gt;${item}&lt;/ring1&gt;`;
+    case 11: return `\t&lt;ring2&gt;${item}&lt;/ring2&gt;`;
+    case 12: return `\t&lt;back&gt;${item}&lt;/back&gt;`;
+    case 13: return `\t&lt;waist&gt;${item}&lt;/waist&gt;`;
+    case 14: return `\t&lt;legs&gt;${item}&lt;/legs&gt;`;
+    case 15: return `\t&lt;feet&gt;${item}&lt;/feet&gt;`;
   }
 }
