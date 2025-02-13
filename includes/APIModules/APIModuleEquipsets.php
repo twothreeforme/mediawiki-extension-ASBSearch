fd45c4a435fdf4e5ab2dea6b0f66b7f81add28ba
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-02-12 20:39:20 -0500

    Update APIModuleEquipsets.php

--- includes/APIModules/APIModuleEquipsets.php
+++ includes/APIModules/APIModuleEquipsets.php
@@ -93,7 +93,7 @@ class APIModuleEquipsets extends ApiBase {
 
             $luaNamesEncoded = base64_encode(json_encode($luaNamesArray));
             $luaNamesURLSafe = urlencode($luaNamesEncoded );
-            $result->addValue( $params['action'], "luaNames", $luaNamesURLSafe );
+            $result->addValue( $params['action'], "luaNames", $luaNamesArray );
 
             //throw new Exception ( "here" );
             // $result->addValue($params['action'], "stats", $newStats->getStats() );
