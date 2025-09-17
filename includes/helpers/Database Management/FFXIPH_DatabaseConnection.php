0ae493bb48b309f3cbe1026c55830af08a55839a
Hugin <bradturner43@gmail.com>
2025-09-17 15:28:59 -0500

    Added database server variable to connection details.

--- includes/helpers/Database Management/FFXIPH_DatabaseConnection.php
+++ includes/helpers/Database Management/FFXIPH_DatabaseConnection.php
@@ -3,21 +3,23 @@
 use Wikimedia\Rdbms\DatabaseFactory;
 
 class DatabaseConnection {
-
-	private $dbUsername;  
-	private $dbPassword;
+    private $dbServer;
+    private $dbUsername;
+    private $dbPassword;
 
     public function __construct() {
+        global $wgDBserver;
         global $wgDBuser;
         global $wgDBpassword;
 
+        $this->dbServer = $wgDBserver;
         $this->dbUsername = $wgDBuser;
         $this->dbPassword = $wgDBpassword;
     }
 
     private function getDatabaseFactory(mixed $database = null): DatabaseMysqli{
         return ( new DatabaseFactory() )->create( 'mysql', [
-                'host' => 'localhost',
+                'host' => $this->dbServer,
                 'user' => $this->dbUsername,
                 'password' => $this->dbPassword,
                 'dbname' => $database,
@@ -27,14 +29,11 @@ class DatabaseConnection {
 
     public function openConnection(mixed $dbName = null): mixed{
         try {
-            $returnDB = $this->getDatabaseFactory($dbName);            
+            $returnDB = $this->getDatabaseFactory($dbName);
         } catch ( DBConnectionError $e ) {
             $status->fatal( 'config-connection-error', $e->getMessage() );
             return $status;
         }
         return $returnDB; 
     }
-
 }
-
-?>
