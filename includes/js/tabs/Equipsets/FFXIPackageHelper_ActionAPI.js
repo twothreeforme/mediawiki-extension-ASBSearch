080383253188bfe86e7dcd05636256900595f5f1
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2025-09-19 16:34:18 -0400

    Update FFXIPackageHelper_ActionAPI.js

--- includes/js/tabs/Equipsets/FFXIPackageHelper_ActionAPI.js
+++ includes/js/tabs/Equipsets/FFXIPackageHelper_ActionAPI.js
@@ -122,7 +122,7 @@ function actionAPI(params, forTab, currentButton, callback) {
         }
         else {
           //updateEquipsets(result['stats']);
-          if ( arrayOfObjects.some(obj => 'stats' in obj) == true ) updateStats(result['stats']);
+          if ( "stats" in result ) updateStats(result['stats']);
         }
       }
       else if ( forTab.includes("fishingsearch") )updateFishingFromQuery(result["fishing"]);
